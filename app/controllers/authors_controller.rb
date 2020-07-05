36f8fdd6f18b143ede029baee7c6a95500f969ac
Megan Horsfield <meganthadev@gmail.com>
2020-07-05 06:46:57 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,5 +3,10 @@ class AuthorsController < ApplicationController
   def show
     @author = Author.find(params[:id])
   end
-
+  
+  def posts_index 
+    @author = Author.find(params[:id])
+    @posts = @author.posts 
+      render template: '/posts/index'
+  end 
 end
